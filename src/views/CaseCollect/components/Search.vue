a9c61d1de147312f360310dc9cf689f36ff2a737
iqiqi <18811464010@163.com>
2020-10-14 11:57:23 +0800

    修复bug

--- src/views/CaseCollect/components/Search.vue
+++ src/views/CaseCollect/components/Search.vue
@@ -3,17 +3,17 @@
     <a-card :bordered="false">
       <div class="table-page-search-wrapper">
         <a-form-model ref="searchForm" layout="inline" :model="searchForm" :rules="rules">
-          <a-row :gutter="searchTableGrid.gutter">
-            <a-col v-bind="searchTableGrid.span">
-              <a-form-model-item label="报表" prop="tableName">
-                <a-select v-model="searchForm.tableName">
+          <a-row :gutter="gutter">
+            <a-col v-bind="span">
+              <a-form-model-item label="报表" prop="tableType">
+                <a-select v-model="searchForm.tableType">
                   <a-select-option v-for="(item,index) in tableType" :key="index" :value="item.code">
                     {{ item.name }}
                   </a-select-option>
                 </a-select>
               </a-form-model-item>
             </a-col>
-            <a-col v-bind="searchTableGrid.span">
+            <a-col v-bind="span">
               <a-form-model-item label="时间" prop="startTime">
                 <a-date-picker v-model="searchForm.startTime" inputReadOnly placeholder="开始时间" @change="(e,str)=>searchForm.startTime=str" style="width:50%">
                   <template #suffixIcon></template>
@@ -23,7 +23,7 @@
                 </a-date-picker>
               </a-form-model-item>
             </a-col>
-            <a-col v-bind="searchTableGrid.span">
+            <a-col v-bind="span">
               <a-form-model-item>
                 <button-export :loading="exportLoading" @click="handleExport" />
               </a-form-model-item>
@@ -53,19 +53,20 @@ export default {
   },
   data() {
     return {
+      span: { lg: 24, xl: 12, xxl: 8 },
       searchForm: {
-        tableName: '',
+        tableType: '',
         startTime: '',
         endTime: ''
       },
       rules: {
-        tableName: [validateRequired]
+        tableType: [validateRequired]
       }
     }
   },
   computed: {
-    searchTableGrid() {
-      return this.$store.getters.searchTableGrid
+    gutter() {
+      return this.$store.getters.searchTableGrid.gutter
     }
   },
   methods: {
@@ -78,5 +79,5 @@ export default {
 }
 </script>
 
-<style>
+<style scoped lang="less">
 </style>
