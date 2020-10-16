b5a794dd85bbcd2f83f7f9b4c9ddd471f187de36
iqiqi <18811464010@163.com>
2020-10-16 13:50:24 +0800

    update

--- src/components/index.js
+++ src/components/index.js
@@ -22,8 +22,6 @@ import Tree from '@/components/Tree/Tree'
 import Trend from '@/components/Trend'
 import STable from '@/components/Table'
 import MultiTab from '@/components/MultiTab'
-import IconSelector from '@/components/IconSelector'
-import TagSelect from '@/components/TagSelect'
 import StandardFormRow from '@/components/StandardFormRow'
 import ArticleListContent from '@/components/ArticleListContent'
 import SettingDrawer from '@/components/SettingDrawer'
@@ -55,8 +53,6 @@ export {
   Tree,
   STable,
   MultiTab,
-  IconSelector,
-  TagSelect,
   StandardFormRow,
   ArticleListContent,
   SettingDrawer,
